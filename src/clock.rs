bb0c1bac30d2e73a9e71b79cb417086b68a2cf8e
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-12-08 08:08:19 +0900

    event.poll()イベントループを回す & ファイル分割

---
+++ src/clock.rs
@@ -0,0 +1,67 @@
+use std::f64::consts::PI;
+
+use ratatui::{
+    layout::Rect,
+    style::{Color, Stylize},
+    text,
+    widgets::canvas::{self, Circle, Context},
+};
+
+#[derive(Debug, Default)]
+pub struct ClockState {
+    pub hour_rad: f64,
+    pub min_rad: f64,
+    pub sec_rad: f64,
+}
+
+pub fn draw(ctx: &mut Context, area: &Rect, state: &ClockState) {
+    ctx.draw(&Circle {
+        x: 0.,
+        y: 0.,
+        radius: 1.,
+        color: Color::DarkGray,
+    });
+    let w = area.width as f64;
+    let h = area.height as f64;
+    // for num in 0..4 {
+    //     ctx.print((-(w / 2.).floor() + num as f64) / (w / 2.), 0., num.to_string());
+    //     ctx.print(((w / 2.).floor() - num as f64) / (w / 2.), 0., num.to_string());
+    //     ctx.print(0., (-(h / 2.).floor() + num as f64) / (h / 2.), num.to_string());
+    //     ctx.print(0., ((h / 2.).floor() - num as f64) / (h / 2.), num.to_string());
+    // }
+    for num in 1..13 {
+        // -w/2+1 <= x <= w/2 の範囲のみ正しく表示される
+        // 四捨五入の境界を回避するために0.5の代わりに0.4999
+        let x = (num as f64 * PI / 6.).sin() * ((w / 2.).ceil() - 2.5) + 0.4999
+            - (if num >= 10 { 0.5 } else { 0. });
+        // -h/2 <= y <= h/2-1 の範囲のみ正しく表示される
+        let y = (num as f64 * PI / 6.).cos() * ((h / 2.).ceil() - 1.5) - 0.4999;
+        ctx.print(
+            x.round() / (w / 2.),
+            y.round() / (h / 2.),
+            text::Line::from(num.to_string()).fg(Color::DarkGray),
+        );
+    }
+
+    ctx.draw(&canvas::Line {
+        x1: 0.,
+        y1: 0.,
+        x2: state.hour_rad.sin() * 0.5,
+        y2: state.hour_rad.cos() * 0.5,
+        color: Color::Red,
+    });
+    ctx.draw(&canvas::Line {
+        x1: 0.,
+        y1: 0.,
+        x2: state.min_rad.sin() * 0.7,
+        y2: state.min_rad.cos() * 0.7,
+        color: Color::Blue,
+    });
+    ctx.draw(&canvas::Line {
+        x1: 0.,
+        y1: 0.,
+        x2: state.sec_rad.sin() * 0.8,
+        y2: state.sec_rad.cos() * 0.8,
+        color: Color::Green,
+    });
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
-use std::f64::consts::PI;
+use std::{f64::consts::PI, time::Duration};
 
 use chrono::Timelike;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use ratatui::{
     DefaultTerminal, Frame,
-    layout::{Constraint, Direction, Layout, Rect},
-    style::{Color, Modifier, Stylize},
-    text::{self, Text},
-    widgets::{
-        Block, Paragraph,
-        canvas::{self, Canvas, Circle, Context},
-    },
+    layout::{Constraint, Direction, Layout},
+    widgets::canvas::Canvas,
 };
 
 fn main() -> color_eyre::Result<()> {
@@ -21,11 +16,14 @@ fn main() -> color_eyre::Result<()> {
     result
 }
 
+mod clock;
+
 /// The main application which holds the state and logic of the application.
 #[derive(Debug, Default)]
 pub struct App {
     /// Is the application running?
     running: bool,
+    clock_state: clock::ClockState,
 }
 
 impl App {
@@ -38,10 +36,21 @@ impl App {
     pub fn run(mut self, mut terminal: DefaultTerminal) -> color_eyre::Result<()> {
         self.running = true;
         while self.running {
+            self.update_chrono();
             terminal.draw(|frame| self.render(frame))?;
             self.handle_crossterm_events()?;
         }
         Ok(())
+    }
+
+    fn update_chrono(&mut self) {
+        let now = chrono::Local::now();
+        let sec = now.second() as f64;
+        let min = now.minute() as f64 + sec / 60.;
+        let hour = now.hour12().1 as f64 + min / 60.;
+        self.clock_state.sec_rad = sec * PI / 30.;
+        self.clock_state.min_rad = min * PI / 30.;
+        self.clock_state.hour_rad = hour * PI / 6.;
     }
 
     /// Renders the user interface.
@@ -81,65 +90,9 @@ impl App {
             Canvas::default()
                 .x_bounds([-1., 1.])
                 .y_bounds([-1., 1.])
-                .paint(|ctx| self.draw_clock(ctx, &canvas_layout)),
+                .paint(|ctx| clock::draw(ctx, &canvas_layout, &self.clock_state)),
             canvas_layout,
         )
-    }
-
-    fn draw_clock(&self, ctx: &mut Context, area: &Rect) {
-        ctx.draw(&Circle {
-            x: 0.,
-            y: 0.,
-            radius: 1.,
-            color: Color::DarkGray,
-        });
-        let w = area.width as f64;
-        let h = area.height as f64;
-        // for num in 0..4 {
-        //     ctx.print((-(w / 2.).floor() + num as f64) / (w / 2.), 0., num.to_string());
-        //     ctx.print(((w / 2.).floor() - num as f64) / (w / 2.), 0., num.to_string());
-        //     ctx.print(0., (-(h / 2.).floor() + num as f64) / (h / 2.), num.to_string());
-        //     ctx.print(0., ((h / 2.).floor() - num as f64) / (h / 2.), num.to_string());
-        // }
-        for num in 1..13 {
-            // -w/2+1 <= x <= w/2 の範囲のみ正しく表示される
-            // 四捨五入の境界を回避するために0.5の代わりに0.4999
-            let x = (num as f64 * PI / 6.).sin() * ((w / 2.).ceil() - 2.5) + 0.4999
-                - (if num >= 10 { 0.5 } else { 0. });
-            // -h/2 <= y <= h/2-1 の範囲のみ正しく表示される
-            let y = (num as f64 * PI / 6.).cos() * ((h / 2.).ceil() - 1.5) - 0.4999;
-            ctx.print(
-                x.round() / (w / 2.),
-                y.round() / (h / 2.),
-                text::Line::from(num.to_string()).fg(Color::DarkGray),
-            );
-        }
-
-        let now = chrono::Local::now();
-        let sec = now.second() as f64;
-        let min = now.minute() as f64 + sec / 60.;
-        let hour = now.hour12().1 as f64 + min / 60.;
-        ctx.draw(&canvas::Line {
-            x1: 0.,
-            y1: 0.,
-            x2: (hour * PI / 6.).sin() * 0.5,
-            y2: (hour * PI / 6.).cos() * 0.5,
-            color: Color::Red,
-        });
-        ctx.draw(&canvas::Line {
-            x1: 0.,
-            y1: 0.,
-            x2: (min * PI / 30.).sin() * 0.7,
-            y2: (min * PI / 30.).cos() * 0.7,
-            color: Color::Blue,
-        });
-        ctx.draw(&canvas::Line {
-            x1: 0.,
-            y1: 0.,
-            x2: (sec * PI / 30.).sin() * 0.8,
-            y2: (sec * PI / 30.).cos() * 0.8,
-            color: Color::Green,
-        });
     }
 
     /// Reads the crossterm events and updates the state of [`App`].
@@ -147,12 +100,14 @@ impl App {
     /// If your application needs to perform work in between handling events, you can use the
     /// [`event::poll`] function to check if there are any events available with a timeout.
     fn handle_crossterm_events(&mut self) -> color_eyre::Result<()> {
-        match event::read()? {
-            // it's important to check KeyEventKind::Press to avoid handling key release events
-            Event::Key(key) if key.kind == KeyEventKind::Press => self.on_key_event(key),
-            Event::Mouse(_) => {}
-            Event::Resize(_, _) => {}
-            _ => {}
+        if event::poll(Duration::from_millis(100))? {
+            match event::read()? {
+                // it's important to check KeyEventKind::Press to avoid handling key release events
+                Event::Key(key) if key.kind == KeyEventKind::Press => self.on_key_event(key),
+                Event::Mouse(_) => {}
+                Event::Resize(_, _) => {}
+                _ => {}
+            }
         }
         Ok(())
     }
